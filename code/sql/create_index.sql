ed041af34402d9ce5fd13d1fbdd4d528cdf16f93
Brighten Wan <bwan006@wch133-08.cs.ucr.edu>
2021-03-17 20:24:42 -0700

    finished indexes

--- code/sql/create_index.sql
+++ code/sql/create_index.sql
@@ -24,27 +24,38 @@ CREATE INDEX cru_arrival_port ON Cruise USING btree(arrival_port);
 CREATE INDEX cru_departure_port ON Cruise USING btree(departure_port);
 
 --ship
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
-CREATE INDEX  
+CREATE INDEX ship_id ON Ship USING btree(id); 
+CREATE INDEX ship_make ON Ship USING btree(make);
+CREATE INDEX ship_model ON Ship USING btree(model);
+CREATE INDEX ship_age ON Ship USING btree(age);
+CREATE INDEX ship_seats ON Ship USING btree(seats);
+
+--technician
+CREATE INDEX tech_id ON Technician USING btree(id); 
+CREATE INDEX tech_fname ON Technician USING btree(full_name);
+
+--Reservation
+CREATE INDEX res_rnum ON Reservation USING btree(rnum); 
+CREATE INDEX res_ccid ON Reservation USING btree(ccid);
+CREATE INDEX res_cid ON Reservation USING btree(cid);
+CREATE INDEX res_status ON Reservation USING btree(status);
+
+--CruiseInfo
+CREATE INDEX info_ciid ON CruiseInfo Using btree(ciid); 
+CREATE INDEX info_cruise_id ON CruiseInfo Using btree(cruise_id);
+CREATE INDEX info_captain_id ON CruiseInfo Using btree(captain_id);
+CREATE INDEX info_ship_id ON CruiseInfo Using btree(ship_id);
+
+--Repairs
+CREATE INDEX rep_rid ON Repairs Using btree(rid);
+CREATE INDEX rep_date ON Repairs Using btree(repair_date);
+CREATE INDEX rep_code ON Repairs Using btree(repair_code);
+CREATE INDEX rep_captain_id ON Repairs Using btree (captain_id);
+CREATE INDEX rep_ship_id ON Repairs USING btree(ship_id);
+CREATE INDEX rep_technician_id ON Repairs USING btree(technician_id);
+
+--Schedule
+CREATE INDEX sche_id ON Schedule USING btree(id);
+CREATE INDEX sche_cruiseNum ON Schedule USING btree(cruiseNum);
+CREATE INDEX sche_departure_time ON Schedule USING btree(departure_time);
+CREATE INDEX sche_arrival_time ON Schedule USING btree(arrival_time);
